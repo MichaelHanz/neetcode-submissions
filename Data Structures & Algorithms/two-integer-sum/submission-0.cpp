d90ecd7a538e08c282e7ce22ae1e822906723836
Michael Hanz <190216149+MichaelHanz@users.noreply.github.com>
2026-06-16 16:45:51 +0800

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> pairIndexes;
+
+        for(int i = 0 ; i < nums.size() ; i++){
+            for(int j = i + 1 ; j < nums.size(); j++){
+
+                if( nums.at(i) + nums.at(j) == target){
+                    pairIndexes.push_back(i);
+                    pairIndexes.push_back(j);
+                }
+            }
+        }
+
+
+
+
+        return pairIndexes;
+    }
+};
