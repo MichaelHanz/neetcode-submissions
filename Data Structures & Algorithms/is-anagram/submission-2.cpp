6ea6abc09cf99537a5be5d3e5bd38bc600278e31
Michael Hanz <190216149+MichaelHanz@users.noreply.github.com>
2026-06-16 16:13:22 +0800

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+
+        int map[26] = { 0 };
+
+        for(char c : s){
+
+            map[c - 'a']++ ;
+        }
+
+        for(char c : t){
+            map[c - 'a']--;
+        }
+
+        for(int x : map){
+            if(x != 0) return false;
+        }
+
+        return true;
+    }
+};
