bde6e5c6d1f45adef24f7fd0d1b1f968e2b19f32
Michael Hanz <190216149+MichaelHanz@users.noreply.github.com>
2026-06-24 18:11:33 +0800

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+        bool IsStrictPalindrome(const string &s , int left, int right){
+
+            while(left < right){
+                if(s[left] != s[right]) {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+    bool validPalindrome(string s) {
+        
+        int left = 0; 
+        int right = s.size() - 1;
+
+        while(left < right){
+            if(s[left] != s[right]){
+
+                bool skipLeft = IsStrictPalindrome(s, left + 1, right);
+
+                bool skipRight = IsStrictPalindrome(s, left, right - 1);
+
+                return skipLeft || skipRight;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+
+        
+    }
+};
